859fb4f12db1eae34cd94b5ecd266aa1c56e8f40
nrogals <nr282@cornell.edu>
2016-07-13 11:02:21 -0400

    Finished Hankel Matricies

--- N4SID_IMPLEMENTATION.m
+++ N4SID_IMPLEMENTATION.m
@@ -1,6 +1,6 @@
 %This the main level script for my implementation of 4SID Algorithms
 
-iterates=1000;
+iterates=10;
 input_vec_length=2;
 A=-0.1*[2 -1 0 ; -1 2 -1 ; 0 -1 2]; 
 
@@ -12,9 +12,17 @@ x0=[0; 0 ; 0];
 
 
 [input_data, output_data, states ] = Linear_System_Data_Gen(A, B, C, D, x0, iterates, input_vec_length); 
-display(input_data); 
 
 j=3; 
+[Up, Up_plus, Uf, Uf_minus] = create_IO_Hankel_Matrix(input_data, j);
+display(output_data); 
+[Yp, Yp_plus, Yf, Yf_minus]= create_IO_Hankel_Matrix(output_data, j);
+
+
+
+
+
+
 
 
 
--- create_IO_Hankel_Matrix.m
+++ create_IO_Hankel_Matrix.m
@@ -1,16 +1,33 @@
-function [Up, Yp, Uf, Uf_minus] = create_IO_Hankel_Matrix( data, j)
+function [Up, Up_plus, Uf, Uf_minus] = create_IO_Hankel_Matrix(data, j)
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
 
-[num_rows, num_columns]=size(input_data); 
+[num_rows, num_columns]=size(data); 
 hankel_matrix=[]; 
-for i = 1:num_rows-1
+for i = 1:num_rows-2
     row=[]; 
     for t= 1:j
-        row(t)=transpose(data(i+t-1));
+        row(:, t)=transpose(data(i+t-1, :));
     end
+    display(row); 
     hankel_matrix=[hankel_matrix; row] ; 
 end
+
+[num_hankel_rows, num_hankel_columns]=size(hankel_matrix); 
+num_hankel_rows=num_hankel_rows/num_columns; 
+i=floor(num_hankel_rows/2)*num_columns;
+display(i); 
+h=i; 
+display(hankel_matrix); 
+
+Up=hankel_matrix(1:i, :); 
+Uf=hankel_matrix(i+1:(i+h), :) ; 
+Up_plus=hankel_matrix(1:i+num_columns, :); 
+Uf_minus=hankel_matrix(i+num_columns+1:(i+h), :); 
+
+
+
+
 
 
 
