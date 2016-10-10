cee2c4492bb9b15645535cea925f1d1032b56634
nrogals <nr282@cornell.edu>
2016-10-10 11:44:48 -0400

    Second Group of Updated Files

---
+++ TEST_FORM_OBSERVABILITY_MATRIX.m
@@ -0,0 +1,14 @@
+%Test Form Observability Matrix 
+
+
+A=-0.1*[2 -1 0 ; -1 2 -1 ; 0 -1 2]; 
+
+
+C=[5 -6 7 ; 8 -9 10 ; 2 4 6; -8 9 2];  
+
+f=3; 
+
+obervability_matrix = form_observability_matrix( C , A , f ); 
+
+
+
